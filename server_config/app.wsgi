c3039e3af9572d3f45b1a805c89c0617abb9dce6
Frank Angeles <angelesfrancisco@angsissti.com>
2024-06-16 20:12:18 -0600

    Update and rename app.wsgi to wsgi.py

--- server_config/app.wsgi
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(0, '/var/www/html/dash_project')
- 
-from app import app as application
---
+++ server_config/wsgi.py
@@ -0,0 +1,5 @@
+# En donde se nombra peak, se tiene que modificar por app
+from peak import app
+
+if __name__ == '__main__':
+    app.run()
